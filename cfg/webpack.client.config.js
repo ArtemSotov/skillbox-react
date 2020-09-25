3d8241b613ce960193df344f6e135b464a75d5c1
Artem Sotov <asotov@mail.ru>
2020-09-25 19:31:30 +0500

    2 home work

--- cfg/webpack.client.config.js
+++ cfg/webpack.client.config.js
@@ -31,8 +31,24 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.[tj]sx?$/,
+        test: /\.[jt]sx?$/,
         use: ["ts-loader"],
+      },
+      {
+        test: /\.less$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: {
+                mode: "local",
+                localIdentName: "[name]__[local]--[hash:base64:5]",
+              },
+            },
+          },
+          "less-loader",
+        ],
       },
     ],
   },
