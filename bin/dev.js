a2c0dfb31bf8b296323c07932de2ff8aaab9a5ba
Artem Sotov <sotov.av@novomet.ru>
2020-11-10 16:53:00 +0500

    разное

--- bin/dev.js
+++ bin/dev.js
@@ -11,48 +11,48 @@ const hmrServer = express();
 const clientCompiler = webpack(webpackClientConfig);
 
 hmrServer.use(
-  webpackDevMiddleware(clientCompiler, {
-    publicPath: webpackClientConfig.output.publicPath,
-    serverSideRender: true,
-    noInfo: true,
-    watchOptions: {
-      ignore: /dist/,
-    },
-    writeToDisk: true,
-    stats: "errors-only",
-  })
+	webpackDevMiddleware(clientCompiler, {
+		publicPath: webpackClientConfig.output.publicPath,
+		serverSideRender: true,
+		noInfo: true,
+		watchOptions: {
+			ignore: /dist/,
+		},
+		writeToDisk: true,
+		stats: "errors-only",
+	})
 );
 
 hmrServer.use(
-  webpackHotMiddleware(clientCompiler, {
-    path: "/static/__webpack_hmr",
-  })
+	webpackHotMiddleware(clientCompiler, {
+		path: "/static/__webpack_hmr",
+	})
 );
 
 hmrServer.listen(3001, () => {
-  console.log("HMR server successful started");
+	console.log("HMR server successful started");
 });
 
 const compiler = webpack(webpackServerConfig);
 // const compiler = webpack(webpackConfig);
 
 compiler.run((err) => {
-  if (err) {
-    console.log("Compilation failed: ", err);
-  }
+	if (err) {
+		console.log("Compilation failed: ", err);
+	}
 
-  compiler.watch({}, (err) => {
-    if (err) {
-      console.log("Compilation failed: ", err);
-    }
-    console.log("Compilation was successfully");
-  });
+	compiler.watch({}, (err) => {
+		if (err) {
+			console.log("Compilation failed: ", err);
+		}
+		console.log("Compilation was successfully");
+	});
 
-  nodemon({
-    script: path.resolve(__dirname, "../dist/server/server.js"),
-    watch: [
-      path.resolve(__dirname, "../dist/server"),
-      path.resolve(__dirname, "../dist/client"),
-    ],
-  });
+	nodemon({
+		script: path.resolve(__dirname, "../dist/server/server.js"),
+		watch: [
+			path.resolve(__dirname, "../dist/server"),
+			path.resolve(__dirname, "../dist/client"),
+		],
+	});
 });
--- src/HomeWorks/Functional.tsx
+++ src/HomeWorks/Functional.tsx
@@ -3,51 +3,51 @@
 import React from "react";
 import { pickFromSyntheticEvent } from "../utils/react/pickFromSyntheticEvent";
 
-const withIdKey = withKey('id');
+// const withIdKey = withKey('id');
 
-function Feed(props: { blocks: IBlockProps[] }) {
-	return (
-		<div>
-			{props.blocks.map(withIdKey(Block))}
-		</div>
-	)
-}
+// function Feed(props: { blocks: IBlockProps[] }) {
+// 	return (
+// 		<div>
+// 			{props.blocks.map(withIdKey(Block))}
+// 		</div>
+// 	)
+// }
 
-interface IBlockProps{
+interface IBlockProps {
 	title: string;
 	id: string;
 }
 
-function Block(props: IBlockProps){
+function Block(props: IBlockProps) {
 	return (
 		<div>{props.title}</div>
 	)
 }
 
-function withKey(key?: string) {
-	return <E, T extends React.ComponentType<E>>(component: T) =>
-		(props: E, index: number) =>
-			React.createElement(
-				component,
-				{ ...props, key: key ? props[key as keyof E] : index },
-				[],
-			);
-}
+// function withKey(key?: string) {
+// 	return <E, T extends React.ComponentType<E>>(component: T) =>
+// 		(props: E, index: number) =>
+// 			React.createElement(
+// 				component,
+// 				{ ...props, key: key ? props[key as keyof E] : index },
+// 				[],
+// 			);
+// }
 
 ////
 
 //function Input(props: {onChange: (value: string) => void, value: string}) {
-function Input({onChange, value}: {onChange: (value: string) => void, value: string}) {
+function Input({ onChange, value }: { onChange: (value: string) => void, value: string }) {
 	return (
 		//<input value={props.value} onChange={(e) => props.onChange(e.currentTarget.value)} />
 		<input value={value} onChange={getValue(onChange)} />
 	)
 }
 
-function Checkbox(props: {onChange: (value: boolean) => void, value: boolean}) {
+function Checkbox(props: { onChange: (value: boolean) => void, value: boolean }) {
 	return (
 		// <input type="checkbox" value={props.value} onChange={(e) => props.onChange(e.currentTarget.value)} />
-		<input type="checkbox" checked={props.value} onChange={getChecked(props.onChange)} />		
+		<input type="checkbox" checked={props.value} onChange={getChecked(props.onChange)} />
 	)
 }
 
