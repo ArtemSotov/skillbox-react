febdbb24cb317427f71312b176a3cdceceee63af
Artem Sotov <sotov.av@novomet.ru>
2020-11-23 15:52:42 +0500

    5.5 ready

--- src/HomeWorks/ComposeExample.tsx
+++ src/HomeWorks/ComposeExample.tsx
@@ -25,3 +25,33 @@ function pipe<U>(...fns: Function[]) {
 	return <E,>(initialValue: any): U =>
 		fns.reduce((previousValue, fn) => fn(previousValue), initialValue);
 }
+
+function pick<K extends string>(prop: K) {
+	return <O extends Record<K, any>>(obj: O) => obj[prop];
+}
+
+const some = pick('value')({ value: 1 }) // -> 1
+
+function isEqual<T>(left: T) {
+	return <E extends T>(rigth: E) => left === rigth;
+}
+
+const comments = [{ id: 22, text: 'one' }, { id: 44, text: 'two' }];
+
+//const filteredComments = comments.filter(({ id }) => id !== 22);// -> [{ id: 44, text: 'two' }]
+const filteredComments = comments.filter(pipe(pick('id'), isEqual(22), cond));// -> [{ id: 44, text: 'two' }]
+
+function cond(b: boolean) {
+	return !b;
+}
+
+const createFilterBy = (prop: string) => (id: number) => pipe(pick(prop), isEqual(id), cond)
+const filterWithId = createFilterBy('id');
+
+//////
+
+const getValueNumber = pipe<number>(
+	pick('currentTarget'),
+	pick('value'),
+	parseInt
+)
