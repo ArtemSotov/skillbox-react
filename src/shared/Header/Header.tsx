dc1cb844a51f634f5c6ed5548c615180a0212443
Artem Sotov <sotov.av@novomet.ru>
2021-01-20 11:45:25 +0500

    8.3, 6:06

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,17 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { tokenContext } from "../context/tokenContext";
 import styles from "./header.css";
 import { SearchBlock } from "./SearchBlock";
 import { SortBlock } from "./SortBlock";
 import { ThreadTitle } from "./ThreadTitle";
 
-interface IHeaderProps {
-	token: string;
-}
-
-export function Header({ token }: IHeaderProps) {
+export function Header() {
+	const { Consumer } = tokenContext;
+	// const token = useContext(tokenContext);
 	return (
 		<header className={styles.header}>
-			<SearchBlock token={token} />
+			<Consumer>{(token) => <SearchBlock token={token} />}</Consumer>
+			{/* <SearchBlock token={token} /> */}
 			<ThreadTitle />
 			<SortBlock />
 		</header>
