7cb4fd7d4fdf9c5704143771047bd21da616e271
Artem Sotov <asotov@mail.ru>
2021-01-20 23:16:01 +0500

    8.3 4:58 Работает Consumer

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React from "react";
 import { tokenContext } from "../context/tokenContext";
 import styles from "./header.css";
 import { SearchBlock } from "./SearchBlock";
@@ -7,11 +7,9 @@ import { ThreadTitle } from "./ThreadTitle";
 
 export function Header() {
 	const { Consumer } = tokenContext;
-	// const token = useContext(tokenContext);
 	return (
 		<header className={styles.header}>
 			<Consumer>{(token) => <SearchBlock token={token} />}</Consumer>
-			{/* <SearchBlock token={token} /> */}
 			<ThreadTitle />
 			<SortBlock />
 		</header>
