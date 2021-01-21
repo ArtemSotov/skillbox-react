8baefdfec35928c7ee6aab5d45a77934586c9f69
Artem Sotov <sotov.av@novomet.ru>
2021-01-21 17:16:07 +0500

    8.4, надо проверить дома

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@
 // https://www.figma.com/file/STABzVueKL3brf4aOgkvW2/Rd-(Mirror)?node-id=94%3A3575
 // Документация к Reddit Api: reddit.com/dev/api/oauth
 
-import React, { Component, useEffect, useState } from "react";
+import React from "react";
 import "./main.global.css";
 import { hot } from "react-hot-loader/root";
 import { Layout } from "./shared/Layout";
@@ -11,19 +11,22 @@ import { Content } from "./shared/Content";
 import { CardsList } from "./shared/CardsList";
 import { useToken } from "./hooks/useToken";
 import { tokenContext } from "./shared/context/tokenContext";
+import { UserContextProvider } from "./shared/context/userContext";
 
 function AppComponent() {
 	const [token] = useToken();
-	const { Provider } = tokenContext;
+	// const { Provider } = tokenContext;
 	return (
-		<Provider value={token}>
-			<Layout>
-				<Header />
-				<Content>
-					<CardsList />
-				</Content>
-			</Layout>
-		</Provider>
+		<tokenContext.Provider value={token}>
+			<UserContextProvider>
+				<Layout>
+					<Header />
+					<Content>
+						<CardsList />
+					</Content>
+				</Layout>
+			</UserContextProvider>
+		</tokenContext.Provider>
 	);
 }
 
