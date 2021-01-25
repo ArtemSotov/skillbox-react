a55c00d7e5a6cab975dde3f55243734ce003d61c
Artem Sotov <sotov.av@novomet.ru>
2021-01-25 15:38:25 +0500

    еще шажок

--- src/shared/CardsList/Card/Preview/Preview.tsx
+++ src/shared/CardsList/Card/Preview/Preview.tsx
@@ -6,7 +6,8 @@ export function Preview() {
 		<div className={styles.preview}>
 			<img
 				className={styles.previewImg}
-				src="https://cdn.dribbble.com/users/44324/screenshots/14489730/media/830a3276fe1e07c8f23f5e112c72c5fb.png"
+				//src="https://cdn.dribbble.com/users/44324/screenshots/14489730/media/830a3276fe1e07c8f23f5e112c72c5fb.png"
+				src="https://external-preview.redd.it/ZbrXwT5ECy9xffhOTtByNZIWoasL7lvhdKmUat87yKc.jpg?auto=webp&amp;s=ecad8d62f6627429630d2a34f8dbef394aea47b0"
 			/>
 		</div>
 	);
--- src/shared/CardsList/CardsList.tsx
+++ src/shared/CardsList/CardsList.tsx
@@ -4,23 +4,39 @@ import styles from "./cardslist.css";
 import axios from "axios";
 import { useToken } from "../../hooks/useToken";
 
-interface IPostsData {
-	text?: string;
+interface IPostData {
+	author?: string;
+	title?: string;
+	url?: string;
+	permalink?: string;
+	preview?: string;
 }
 
 export function CardsList() {
 	const [token] = useToken();
-	const [data, setData] = useState<IPostsData>({});
+	const [data, setData] = useState<IPostData[]>([]);
 	useEffect(() => {
 		axios
 			.get("https://oauth.reddit.com/best.json", {
 				headers: { Authorization: `bearer ${token}` },
 			})
 			.then((resp) => {
-				console.log("resp: ", resp);
-				const postsData = resp.data;
-				console.log("postsData: ", postsData);
-				setData({ text: "lalala" });
+				const respList: any[] = resp.data.data.children;
+				console.log("respList: ", respList);
+
+				const dataList: IPostData[] = [];
+				for (let i = 0; i < respList.length; i++) {
+					dataList.push({
+						author: respList[i].data.author,
+						title: respList[i].data.title,
+						url: respList[i].data.url,
+						permalink: respList[i].data.permalink,
+						preview: respList[i].data?.preview?.images[0]?.source?.url,
+					});
+				}
+				console.log("dataList: ", dataList);
+
+				setData(dataList);
 			})
 			.catch(console.log);
 	}, []);
