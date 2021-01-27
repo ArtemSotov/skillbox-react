d0fad995558337cab7b0abe535dcd95257ef13ed
Artem Sotov <sotov.av@novomet.ru>
2021-01-27 09:15:10 +0500

    Кажется готово, но надо дома проверить

---
+++ src/hooks/usePostsData.ts
@@ -0,0 +1,38 @@
+import axios from "axios";
+import { useState, useEffect, useContext } from "react";
+import { tokenContext } from "../shared/context/tokenContext";
+
+interface IPost {
+	author?: string;
+	title?: string;
+	url?: string;
+	permalink?: string;
+	preview?: string;
+}
+
+export function usePostsData() {
+	const [data, setData] = useState<IPost[]>([]);
+	const token = useContext(tokenContext);
+	useEffect(() => {
+		axios
+		.get("https://oauth.reddit.com/best.json", {
+			headers: { Authorization: `bearer ${token}` },
+		})
+		.then((resp) => {
+			const respList: any[] = resp.data.data.children;
+			const dataList: IPost[] = [];
+			for (let i = 0; i < respList.length; i++) {
+				dataList.push({
+					author: respList[i].data.author,
+					title: respList[i].data.title,
+					url: respList[i].data.url,
+					permalink: respList[i].data.permalink,
+					preview: respList[i].data?.preview?.images[0]?.source?.url,
+				});
+			}
+			setData(dataList);
+		})
+		.catch(console.log);
+	}, [token]);
+	return [data];
+}
--- src/shared/CardsList/CardsList.tsx
+++ src/shared/CardsList/CardsList.tsx
@@ -1,50 +1,19 @@
-import React, { useEffect, useState } from "react";
+import React, { useContext } from "react";
+import { postsContext } from "../context/postsContext";
 import { Card } from "./Card/Card";
 import styles from "./cardslist.css";
-import axios from "axios";
-import { useToken } from "../../hooks/useToken";
-import { Text } from "../Text";
-
-interface IPost {
-	author?: string;
-	title?: string;
-	url?: string;
-	permalink?: string;
-	preview?: string;
-}
 
 export function CardsList() {
-	const [token] = useToken();
-	const [data, setData] = useState<IPost[]>([]);
-	useEffect(() => {
-		axios
-			.get("https://oauth.reddit.com/best.json", {
-				headers: { Authorization: `bearer ${token}` },
-			})
-			.then((resp) => {
-				const respList: any[] = resp.data.data.children;
-				const dataList: IPost[] = [];
-				for (let i = 0; i < respList.length; i++) {
-					dataList.push({
-						author: respList[i].data.author,
-						title: respList[i].data.title,
-						url: respList[i].data.url,
-						permalink: respList[i].data.permalink,
-						preview: respList[i].data?.preview?.images[0]?.source?.url,
-					});
-				}
-				console.log("dataList: ", dataList);
-
-				setData(dataList);
-			})
-			.catch(console.log);
-	}, []);
-	const url = "https://external-preview.redd.it/ZbrXwT5ECy9xffhOTtByNZIWoasL7lvhdKmUat87yKc.jpg?auto=webp&s=ecad8d62f6627429630d2a34f8dbef394aea47b0";
-	console.log("data: ", data);
+	const cardsList = useContext(postsContext);
 	return (
 		<ul className={styles.cardsList}>
-			{data.map((p) => (
-				<Card preview={p.preview?.replace("&amp;", "&")} title={p.title} author={p.author} url={p.url} />
+			{cardsList.map((p) => (
+				<Card
+					preview={p.preview?.replace("&amp;", "&")}
+					title={p.title}
+					author={p.author}
+					url={p.url}
+				/>
 			))}
 		</ul>
 	);
